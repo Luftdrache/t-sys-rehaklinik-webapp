d2819fe51f3bbbe17904f086f09602d667a4580b
Julia <ydalskaya@gmail.com>
2020-07-11 23:08:16 +0300

    JPA Configuration added

---
+++ src/main/java/com/tsystems/rehaklinik/configuration/JPAConfig.java
@@ -0,0 +1,73 @@
+package com.tsystems.rehaklinik.configuration;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@ComponentScan(basePackages = "com.tsystems.rehaklinik")
+@EnableTransactionManagement
+@PropertySource("classpath:database.properties")
+public class JPAConfig {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        dataSource.setUsername(environment.getProperty("db.username"));
+        dataSource.setPassword(environment.getProperty("db.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+        hibernateProperties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+        hibernateProperties.put("hibernate.max_fetch_depth", environment.getRequiredProperty("hibernate.max_fetch_depth"));
+        hibernateProperties.put("hibernate.max_fetch_size", environment.getRequiredProperty("hibernate.max_fetch_size"));
+        hibernateProperties.put("hibernate.max_batch_size", environment.getRequiredProperty("hibernate.max_batch_size"));
+        return hibernateProperties;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
+
+
+    @Bean(destroyMethod = "")
+    public EntityManagerFactory entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        factoryBean.setJpaProperties(hibernateProperties());
+        factoryBean.setPackagesToScan("com.tsystems.rehaklinik.entities");
+        return factoryBean.getNativeEntityManagerFactory();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+       return new JpaTransactionManager(entityManagerFactory());
+    }
+}
