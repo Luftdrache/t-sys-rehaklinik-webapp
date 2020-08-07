64b8e31dcbd5695503f23ee795163fe75f9e3903
Julia <ydalskaya@gmail.com>
2020-08-07 21:21:34 +0300

    CustomAuthenticationSuccessHandler added

--- src/main/java/com/tsystems/rehaklinik/configuration/SecurityConfig.java
+++ src/main/java/com/tsystems/rehaklinik/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.tsystems.rehaklinik.configuration;
 
 import com.tsystems.rehaklinik.security.CustomAuthProviderImpl;
+import com.tsystems.rehaklinik.security.CustomAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +10,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 
 @Configuration
@@ -36,6 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage(LOGIN_PAGE)
                 .loginProcessingUrl("/login/process") //login form should POST data to this URL for processing.Here SS checks login and pass
                 .permitAll()
+                .successHandler(customAuthenticationSuccessHandler())
                 .and()
                 .logout().permitAll()
                 .and()
@@ -46,6 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authProvider);
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler(){
+        return new CustomAuthenticationSuccessHandler();
     }
 
     @Autowired
