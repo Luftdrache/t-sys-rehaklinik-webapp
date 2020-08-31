463fd60bbc482f1fa323a8a0e425f4dfc87ad66e
Julia <ydalskaya@gmail.com>
2020-08-31 23:29:37 +0300

    Properties file for JMS added

--- src/main/java/com/tsystems/rehaklinik/configuration/JMSConfig.java
+++ src/main/java/com/tsystems/rehaklinik/configuration/JMSConfig.java
@@ -1,10 +1,12 @@
 package com.tsystems.rehaklinik.configuration;
 
 
-import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jms.connection.UserCredentialsConnectionFactoryAdapter;
 import org.springframework.jms.core.JmsTemplate;
 
@@ -16,31 +18,25 @@ import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = "com.tsystems.rehaklinik")
+@PropertySource("classpath:jms.properties")
 public class JMSConfig {
 
-    public static final String CONTEXT_FACTORY = "org.wildfly.naming.client.WildFlyInitialContextFactory";
-    public static final String JMS_CONNECTION_FACTORY_JNDI = "jms/RemoteConnectionFactory";
-    public static final String WILDFLY_REMOTING_URL = "http-remoting://127.0.0.1:8080";
-    public static final String JMS_USERNAME = "luftdrache";
-    public static final String JMS_PASSWORD = "luftdrache";
-    public static final String JMS_QUEUE_JNDI = "treatmentEventQueue";
-//    public static final String JMS_TOPIC_QUEUE_JNDI = "treatmentEventTopicQueue";
-
+    private final Environment environment;
 
     @Bean
     public ConnectionFactory connectionFactory() throws NamingException {
         Properties properties = new Properties();
-        properties.put(Context.INITIAL_CONTEXT_FACTORY, CONTEXT_FACTORY);
-        properties.put(Context.PROVIDER_URL, WILDFLY_REMOTING_URL);
-        properties.put(Context.SECURITY_PRINCIPAL, JMS_USERNAME);
-        properties.put(Context.SECURITY_CREDENTIALS, JMS_PASSWORD);
-
+        properties.put(Context.INITIAL_CONTEXT_FACTORY, environment.getRequiredProperty("jms.context.factory"));
+        properties.put(Context.PROVIDER_URL, environment.getRequiredProperty("jms.provider.url"));
+        properties.put(Context.SECURITY_PRINCIPAL, environment.getRequiredProperty("jms.username"));
+        properties.put(Context.SECURITY_CREDENTIALS, environment.getRequiredProperty("jms.password"));
         InitialContext initialContext = new InitialContext(properties);
 
-        ConnectionFactory connectionFactory = (ConnectionFactory) initialContext.lookup(JMS_CONNECTION_FACTORY_JNDI);
+        ConnectionFactory connectionFactory =
+                (ConnectionFactory) initialContext.lookup(environment.getRequiredProperty("jms.connection.factory.jndi"));
         UserCredentialsConnectionFactoryAdapter connectionFactoryAdapter = new UserCredentialsConnectionFactoryAdapter();
-        connectionFactoryAdapter.setUsername(JMS_USERNAME);
-        connectionFactoryAdapter.setPassword(JMS_PASSWORD);
+        connectionFactoryAdapter.setUsername(environment.getRequiredProperty("jms.username"));
+        connectionFactoryAdapter.setPassword(environment.getRequiredProperty("jms.password"));
         connectionFactoryAdapter.setTargetConnectionFactory(connectionFactory);
         return connectionFactoryAdapter;
     }
@@ -49,10 +45,12 @@ public class JMSConfig {
     public JmsTemplate jmsTemplate() throws NamingException {
         JmsTemplate template = new JmsTemplate();
         template.setConnectionFactory(connectionFactory());
-        template.setDefaultDestinationName(JMS_QUEUE_JNDI);
+        template.setDefaultDestinationName(environment.getRequiredProperty("jms.queue.jndi"));
         return template;
     }
+
+    @Autowired
+    public JMSConfig(Environment environment) {
+        this.environment = environment;
+    }
 }
-//https://docs.jboss.org/author/display/WFLY/Application%20Client%20Migration.html
-//http://www.mastertheboss.com/jboss-server/jboss-jms/jboss-jms-configuration
-//https://access.redhat.com/documentation/en-us/red_hat_jboss_enterprise_application_platform/7.0/html/configuring_messaging/configure_destinations_artemis
